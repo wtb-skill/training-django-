39a8d782fffac050187e79cfe96cad60ddb21ce9
Adam <adambaldyga@gmail.com>
2024-06-05 15:19:09 +0200

    Add basic dashboard app with functional html/css code

--- training_project/dashboard/views.py
+++ training_project/dashboard/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.db.models import F
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+
+class DashboardView(generic.TemplateView):
+    template_name = "dashboard/dashboard.html"
+
