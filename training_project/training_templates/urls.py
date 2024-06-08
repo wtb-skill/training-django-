12f1a71a92a27a835d470408310a83fbae673222
Adam <adambaldyga@gmail.com>
2024-06-08 16:02:19 +0200

    Add working 'training templates' without js but pseudo dynamic

--- training_project/training_templates/urls.py
+++ training_project/training_templates/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_training_template, add_exercise, training_template_list
+from .views import create_training_template, add_exercise, finish_template, training_template_list
 
 urlpatterns = [
     path('create/', create_training_template, name='create_training_template'),
     path('add-exercise/', add_exercise, name='add_exercise'),
+    path('finish/', finish_template, name='finish_template'),
     path('', training_template_list, name='training_template_list'),
 ]
--- training_project/training_templates/views.py
+++ training_project/training_templates/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import TrainingTemplate
 from .forms import TrainingTemplateForm, AddExerciseForm
@@ -6,17 +6,29 @@ from exercises.models import Exercise
 
 
 def create_training_template(request):
+    training_template = None
+    if 'training_template_id' in request.session:
+        training_template_id = request.session['training_template_id']
+        training_template = TrainingTemplate.objects.get(id=training_template_id)
+
     if request.method == 'POST':
-        form = TrainingTemplateForm(request.POST)
+        form = TrainingTemplateForm(request.POST, instance=training_template)
         if form.is_valid():
             training_template = form.save()
             request.session['training_template_id'] = training_template.id
             return redirect('create_training_template')  # Redirect to the same page to add exercises
     else:
-        form = TrainingTemplateForm()
+        form = TrainingTemplateForm(instance=training_template)
 
     exercises = Exercise.objects.all()
-    return render(request, 'training_templates/create_training_template.html', {'form': form, 'exercises': exercises})
+    selected_exercises = training_template.exercises.all() if training_template else []
+
+    return render(request, 'training_templates/create_training_template.html', {
+        'form': form,
+        'exercises': exercises,
+        'selected_exercises': selected_exercises
+    })
+
 
 
 def add_exercise(request):
@@ -31,12 +43,15 @@ def add_exercise(request):
         if form.is_valid():
             exercise = form.cleaned_data['exercise']
             training_template.exercises.add(exercise)
-            if 'finish' in request.POST:
-                del request.session['training_template_id']
-                return redirect('training_template_list')
-            return JsonResponse({'success': True, 'exercise_name': exercise.name})
+            return redirect('create_training_template')  # Redirect to the same page to add more exercises
 
-    return JsonResponse({'success': False, 'error': 'Invalid data'})
+    return redirect('create_training_template')
+
+
+def finish_template(request):
+    if 'training_template_id' in request.session:
+        del request.session['training_template_id']
+    return redirect('training_template_list')
 
 
 def training_template_list(request):
