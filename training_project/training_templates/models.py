0b984bca3102472ab9c229e6e058178c08c4bee0
Adam <adambaldyga@gmail.com>
2024-06-11 10:18:40 +0200

    Edit ReadMe and revert model changes

--- training_project/training_templates/models.py
+++ training_project/training_templates/models.py
@@ -12,15 +12,3 @@ class TrainingTemplate(models.Model):
         exercises_list = self.exercises.all()
         exercises_str = "\n".join([f"Exercise {idx + 1}: {exercise.name}" for idx, exercise in enumerate(exercises_list)])
         return f"Name: {self.name}\n{exercises_str}"
-
-
-class TrainingTemplateExercise(models.Model):
-    training_template = models.ForeignKey(TrainingTemplate, on_delete=models.CASCADE)
-    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
-    order = models.PositiveIntegerField()
-
-    class Meta:
-        ordering = ['order']
-
-    def __str__(self):
-        return f"{self.training_template.name} - {self.exercise.name} - {self.order}"
