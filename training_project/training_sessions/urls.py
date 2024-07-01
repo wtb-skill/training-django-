21047649938ca413feb072c906099ac55ae39ead
Adam <adambaldyga@gmail.com>
2024-07-01 12:27:03 +0200

    Add processing of sessions and save to db

--- training_project/training_sessions/urls.py
+++ training_project/training_sessions/urls.py
@@ -1,8 +1,9 @@
 # training_session/urls.py
 from django.urls import path
-from .views import StartTrainingSessionView
+from .views import StartTrainingSessionView, ProcessTrainingSessionView
 
 urlpatterns = [
     path('start/<int:template_id>/', StartTrainingSessionView.as_view(), name='start_training_session'),
+    path('process/<int:session_id>/', ProcessTrainingSessionView.as_view(), name='process_training_session'),
 ]
 
--- training_project/training_sessions/views.py
+++ training_project/training_sessions/views.py
@@ -1,6 +1,8 @@
 # training_session/views.py
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+from django.utils import timezone
+from django.urls import reverse
 from .models import TrainingSession, SessionExercise, SessionSet
 from training_templates.models import TrainingTemplate, ExerciseOrder, Set
 
@@ -10,44 +12,55 @@ class StartTrainingSessionView(View):
         training_template_id = kwargs.get('template_id')
         training_template = get_object_or_404(TrainingTemplate, pk=training_template_id)
 
-        # Get exercise orders with related exercises
+        # Create a new TrainingSession
+        training_session = TrainingSession.objects.create(template=training_template)
+
+        # Create session exercises and sets
         exercise_orders = ExerciseOrder.objects.filter(training_template=training_template).select_related('exercise')
-
-        # Prepare a dictionary to hold exercises and their sets
-        exercises_with_sets = []
         for exercise_order in exercise_orders:
+            session_exercise = SessionExercise.objects.create(session=training_session,
+                                                              exercise=exercise_order.exercise)
             sets = Set.objects.filter(exercise_order=exercise_order)
-            exercises_with_sets.append({
-                'exercise_order': exercise_order,
-                'sets': sets
-            })
+            for set in sets:
+                SessionSet.objects.create(session_exercise=session_exercise, set_type=set.set_type, reps=set.reps,
+                                          weight=set.weight)
 
+        # Pass the newly created session ID to the context
         context = {
             'training_template': training_template,
-            'exercises_with_sets': exercises_with_sets,
+            'exercises_with_sets': [
+                {
+                    'exercise_order': eo,
+                    'sets': SessionSet.objects.filter(session_exercise__exercise=eo.exercise,
+                                                      session_exercise__session=training_session)
+                } for eo in exercise_orders
+            ],
+            'session_id': training_session.id,
         }
 
         return render(request, 'training_sessions/start.html', context)
 
 
-# class ProcessTrainingSessionView(View):
-#     def get(self, request, session_id):
-#         session = TrainingSession.objects.get(id=session_id)
-#         return render(request, 'training_session/start.html', {'session': session})
-#
-#     def post(self, request, session_id):
-#         session = TrainingSession.objects.get(id=session_id)
-#         for exercise in session.session_exercises.all():
-#             exercise.is_completed = request.POST.get(f'exercise_{exercise.id}_completed') == 'on'
-#             exercise.save()
-#             for set in exercise.session_sets.all():
-#                 set.is_completed = request.POST.get(f'set_{set.id}_completed') == 'on'
-#                 set.save()
-#
-#         session.is_completed = all(ex.is_completed for ex in session.session_exercises.all())
-#         if session.is_completed:
-#             session.end_time = timezone.now()
-#         session.save()
-#
-#         return redirect('some_finish_view')
+class ProcessTrainingSessionView(View):
+    def post(self, request, *args, **kwargs):
+        session_id = kwargs.get('session_id')
+        training_session = get_object_or_404(TrainingSession, pk=session_id)
 
+        for key, value in request.POST.items():
+            if key.startswith('set_') and key.endswith('_reps'):
+                set_id = key.split('_')[1]
+                session_set = get_object_or_404(SessionSet, pk=set_id)
+                session_set.reps = int(value)
+                session_set.save()
+            elif key.startswith('set_') and key.endswith('_weight'):
+                set_id = key.split('_')[1]
+                session_set = get_object_or_404(SessionSet, pk=set_id)
+                session_set.weight = float(value)
+                session_set.save()
+
+        # Mark the session as completed and set the end time
+        training_session.is_completed = True
+        training_session.end_time = timezone.now()
+        training_session.save()
+
+        return redirect('/')
