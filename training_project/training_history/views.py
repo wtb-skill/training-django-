fc14d4a86cb2f2c4feb373220eda23994387fecb
Adam <adambaldyga@gmail.com>
2024-11-27 14:50:56 +0100

    Add session length to training_history app (still WIP)

--- training_project/training_history/views.py
+++ training_project/training_history/views.py
@@ -2,6 +2,7 @@
 
 from django.views.generic import ListView
 from training_sessions.models import TrainingSession
+from django.utils import timezone
 
 class TrainingSessionHistoryView(ListView):
     model = TrainingSession
@@ -10,5 +11,26 @@ class TrainingSessionHistoryView(ListView):
     ordering = ['-start_time']
 
     def get_queryset(self):
-        # Optionally add some filtering or prefetching of related data
-        return super().get_queryset().prefetch_related('session_exercises__session_sets')
+        queryset = super().get_queryset().prefetch_related('session_exercises__session_sets')
+
+        # Add duration details to the session
+        for session in queryset:
+            if session.start_time and session.end_time:
+                session.duration = session.end_time - session.start_time
+            elif session.start_time:
+                # If the session is still ongoing, calculate the duration until now
+                session.duration = timezone.now() - session.start_time
+            else:
+                session.duration = None
+
+            # Precompute hours, minutes, seconds for easier template rendering
+            if session.duration:
+                total_seconds = session.duration.total_seconds()
+                session.hours = int(total_seconds // 3600)
+                session.minutes = int((total_seconds % 3600) // 60)
+                session.seconds = int(total_seconds % 60)
+            else:
+                session.hours = session.minutes = session.seconds = 0
+
+        return queryset
+
